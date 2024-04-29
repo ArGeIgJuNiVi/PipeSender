4f65c7bd6965f800b3400ef337e021f28193adb8
Nicon-Georgian Albu <nicon@containrr.com>
2024-04-29 11:23:05 +0200

    pipe sender with initial length int

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+using System.IO.Pipes;
+
+namespace PipeSender;
+
+public class _
+{
+    public static void Main(string[] args)
+    {
+        using AnonymousPipeServerStream pipe = new(PipeDirection.Out, HandleInheritability.Inheritable);
+
+        Process client = new();
+        client.StartInfo.UseShellExecute = false;
+        client.StartInfo.FileName = "../PipeReceiver/bin/Debug/net8.0/PipeReceiver";
+        client.StartInfo.Arguments = pipe.GetClientHandleAsString();
+        client.Start();
+
+        byte[] pipedValue = [.. string.Join("", args).ToCharArray().Select((val) => (byte)val)];
+        pipe.Write(BitConverter.GetBytes(pipedValue.Length));
+        pipe.Write(pipedValue);
+
+        client.WaitForExit();
+        client.Close();
+    }
+}
